641b4c0d460ea775bce639330775ccbbe898a663
14likhit <likhit.u@collpoll.com>
2020-06-29 04:55:19 +0530

    Added Application class.Updated Same In Manifest. Added Internet Permission. [Ainak]->Application class to initialise instances at app level. [AndroidManifest]->Added Internet Permission. Added application class name.

---
+++ app/src/main/java/com/example/ainak/Ainak.java
@@ -0,0 +1,25 @@
+package com.example.ainak;
+
+import android.app.Application;
+
+import androidx.multidex.BuildConfig;
+
+import com.example.ainak.network.ApiClient;
+
+import timber.log.Timber;
+
+public class Ainak extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Initializing Retrofit Instance
+        ApiClient.Companion.initialize(this);
+
+        //Initializing Timber for Debug
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
