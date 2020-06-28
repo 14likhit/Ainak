8ae9cf593b1cbdfb080bbeb8b83aecb2ac5f47ac
14likhit <likhit.u@collpoll.com>
2020-06-29 04:45:40 +0530

    Added base classes and layouts. [BaseActivity]->Parent Class for all activities. [BaseFragment]->Parent Class for all fragments. [Utils]->Utility Class for the application. [layout_divider]->basic divider view. [toolbar_default]->Default Toolbar for the application.

---
+++ app/src/main/java/com/example/ainak/utils/Utils.kt
@@ -0,0 +1,39 @@
+package com.example.ainak.utils
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+/**
+ * Utility class.
+ */
+class Utils {
+
+    companion object {
+
+        fun pxToDp(context: Context, px: Float): Float {
+            return px / context.resources.displayMetrics.density
+        }
+
+        fun dpToPx(context: Context, dp: Float): Float {
+            return dp * context.resources.displayMetrics.density
+        }
+
+        fun loadImage(
+            context: Context?,
+            uri: String?,
+            placeholder: Int,
+            error: Int,
+            imageView: ImageView?
+        ) {
+            Glide.with(context!!)
+                .load(uri)
+                .placeholder(placeholder)
+                .error(error)
+                .centerCrop()
+                .into(imageView!!)
+        }
+
+    }
+
+}
