7a1034ac5c784a73d7de19860a5d0fb64f128625
14likhit <likhit.u@collpoll.com>
2020-06-29 05:01:29 +0530

    Updated Name of Main Activity to Splash.Added ui package.

---
+++ app/src/main/java/com/example/ainak/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ainak.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ainak.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
