35e54db2c6f9fb49532a7fa1d11c644e0a1b4843
14likhit <likhit.u@collpoll.com>
2020-06-29 14:15:28 +0530

    added image List adapter. [ImageListAdapter]->added image List adapter.

---
+++ app/src/main/java/com/example/ainak/adapter/ImageListAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.ainak.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ainak.R
+import com.example.ainak.data.models.Photo
+import com.example.ainak.databinding.LayoutImageItemBinding
+import com.example.ainak.utils.Utils
+
+/**
+ * Adapter for Image List
+ */
+
+class ImageListAdapter(private val mContext: Context) :
+    RecyclerView.Adapter<ImageListAdapter.ImageListViewHolder>() {
+
+    var images: MutableList<Photo>? = null
+
+    var layoutInflater: LayoutInflater? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageListViewHolder {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.context)
+        }
+        return ImageListViewHolder(
+            LayoutImageItemBinding.inflate(
+                layoutInflater!!,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return if (images == null) 0 else images?.size!!
+    }
+
+    override fun onBindViewHolder(holder: ImageListViewHolder, position: Int) {
+        if (images != null) {
+            var photo = images!![position]
+            Utils.loadImage(
+                mContext,
+                photo.imageUrl,
+                R.drawable.ic_logo,
+                R.drawable.ic_logo,
+                holder.binding.imageItemView
+            )
+        }
+    }
+
+    class ImageListViewHolder(val binding: LayoutImageItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
