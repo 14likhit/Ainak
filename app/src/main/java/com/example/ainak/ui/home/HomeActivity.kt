28f2b8b398eaf8bcbe4bae56740a5fae7f563f34
14likhit <likhit.u@collpoll.com>
2020-06-29 14:35:07 +0530

    Added logic in HomeActivity.Added ids in activity_home view. [HomeActivity]->Added logic. [activity_home]-Added Ids for the views.

--- app/src/main/java/com/example/ainak/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/ainak/ui/home/HomeActivity.kt
@@ -1,13 +1,73 @@
 package com.example.ainak.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.ainak.R
+import com.example.ainak.adapter.ImageListAdapter
+import com.example.ainak.base.BaseActivity
+import com.example.ainak.data.models.Photo
+import com.example.ainak.data.remote.RemoteRepositoryClass
+import com.example.ainak.databinding.ActivityHomeBinding
+import com.example.ainak.network.ApiClient
+import com.example.ainak.viewmodelfactories.HomeViewModelFactory
+import com.example.ainak.viewmodels.HomeViewModel
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
+
+    private lateinit var activityHomeBinding: ActivityHomeBinding
+
+    private lateinit var homeViewModelFactory: HomeViewModelFactory
+    private lateinit var homeViewModel: HomeViewModel
+
+    private var images: ArrayList<Photo>? = null
+    private var imageListAdapter: ImageListAdapter? = null
+
+    private var imagesLayoutManager: GridLayoutManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        activityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        homeViewModelFactory =
+            HomeViewModelFactory(
+                RemoteRepositoryClass.getInstance(ApiClient.getInstance()?.getAinakApi()!!)!!
+            )
+
+        homeViewModel =
+            ViewModelProvider(
+                this,
+                homeViewModelFactory
+            ).get(HomeViewModel::class.java)
+
+        addObservers()
+
+        initView()
+
+    }
+
+    private fun addObservers() {
+
+    }
+
+    private fun initView() {
+        if (imageListAdapter == null) {
+            imageListAdapter = ImageListAdapter(this)
+        }
+
+        if (images == null) {
+            images = ArrayList()
+        }
+
+        imagesLayoutManager = GridLayoutManager(this, 2)
+
+        activityHomeBinding.homeImageListLayout.imageListRecyclerView.layoutManager =
+            imagesLayoutManager
+        activityHomeBinding.homeImageListLayout.imageListRecyclerView.itemAnimator =
+            DefaultItemAnimator()
+        activityHomeBinding.homeImageListLayout.imageListRecyclerView.adapter = imageListAdapter
+
     }
 }
