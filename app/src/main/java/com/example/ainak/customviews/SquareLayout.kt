02b78a721429f34185ed635075ccc8d4dbd9f283
14likhit <likhit.u@collpoll.com>
2020-06-29 14:05:59 +0530

    Added custom view Square layout for images and layout_image_item for single image. added logo drawable. [SquareLayout]->Custom View for images. [ic_logo]->logo drawable. [layout_image_item]->layout for image item.

---
+++ app/src/main/java/com/example/ainak/customviews/SquareLayout.kt
@@ -0,0 +1,24 @@
+package com.example.ainak.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+
+internal class SquareLayout : RelativeLayout {
+
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+}
