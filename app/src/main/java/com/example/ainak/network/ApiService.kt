9933e107ca4a2c6325043934d60b4860f34d502c
14likhit <likhit.u@collpoll.com>
2020-06-29 06:03:53 +0530

    Added Api to fetch Image. [ApiService]->Added Api to fetch Images.

--- app/src/main/java/com/example/ainak/network/ApiService.kt
+++ app/src/main/java/com/example/ainak/network/ApiService.kt
@@ -1,9 +1,17 @@
 package com.example.ainak.network;
 
+import com.example.ainak.data.models.ImagesRequestBody
+import com.google.gson.JsonElement
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.Body
+import retrofit2.http.GET
+
 /**
  * Service to implement all Api endpoints
  */
 interface ApiService {
 
+    @GET("services/rest/")
+    fun getImages(@Body imagesRequestBody: ImagesRequestBody): Single<JsonElement>
 
 }
