f0587b95fabe8feb62ef10af05132ea2042c641f
14likhit <likhit.u@collpoll.com>
2020-06-29 05:39:44 +0530

    Added logic to show loading on splash screen.Added dimen resource file. [SplashActivity]->Added logic to show loading on splash screen.

--- app/src/main/java/com/example/ainak/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/ainak/ui/splash/SplashActivity.kt
@@ -1,13 +1,31 @@
 package com.example.ainak.ui.splash
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.ainak.R
+import com.example.ainak.base.BaseActivity
+import com.example.ainak.utils.ActivityLauncher
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity() {
+
+    private val timerValueSeconds: Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        startTimer()
+    }
+
+    private fun startTimer() {
+        Observable.timer(timerValueSeconds, TimeUnit.MILLISECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                ActivityLauncher.launchHomeActivity(this@SplashActivity)
+            }
     }
 }
