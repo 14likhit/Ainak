d17b47fc3971af8498065e5db0a0e5515d789c4f
14likhit <likhit.u@collpoll.com>
2020-06-29 16:26:46 +0530

    Added SlideShowImageAdapter and image_full_screen_preview.Added dimens and strings.

---
+++ app/src/main/java/com/example/ainak/adapter/SlideShowImageAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.ainak.adapter
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager.widget.PagerAdapter
+import com.example.ainak.R
+import com.example.ainak.data.models.Photo
+import com.example.ainak.databinding.ImageFullScreenPreviewBinding
+import com.example.ainak.utils.Utils
+
+
+class SlideShowImageAdapter(private var activity: Activity, private var images: ArrayList<Photo>) :
+    PagerAdapter() {
+
+    private var layoutInflater: LayoutInflater? = null
+
+    private lateinit var binding: ImageFullScreenPreviewBinding
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater =
+            activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater?
+        val view: View =
+            layoutInflater!!.inflate(R.layout.image_full_screen_preview, container, false)
+        binding = DataBindingUtil.bind(view)!!
+
+        Utils.loadImage(
+            activity,
+            images[position].imageUrl,
+            R.drawable.ic_logo,
+            R.drawable.ic_logo,
+            binding.photoImageView
+        )
+
+        container.addView(view)
+
+        return view
+    }
+
+    override fun isViewFromObject(view: View, obj: Any): Boolean {
+        return view === obj as View
+    }
+
+    override fun getCount(): Int {
+        return images.size
+    }
+
+
+}
